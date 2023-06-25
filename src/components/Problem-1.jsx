a2902af1a7715dacf9e5e76c827a6e8b840dbbbb
Md Arefine Ahamed Joy <mdarefine05@gmail.com>
2023-06-25 17:57:15 +0600

    add input data and short iinpurt data

--- src/components/Problem-1.jsx
+++ src/components/Problem-1.jsx
@@ -1,12 +1,47 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 
 const Problem1 = () => {
 
     const [show, setShow] = useState('all');
+    const [inputData , setInputData] = useState([])
+    const [filterData , setFilterData] = useState([])
+
 
     const handleClick = (val) =>{
         setShow(val);
     }
+
+
+    const handelInputValue = (e) =>{
+        e.preventDefault()
+        const form = e.target 
+        const name = form.name.value
+        const status = form.status.value
+        const datas = {name , status}
+        setInputData([...inputData, datas])
+        form.reset()
+    }
+
+    useEffect(()=>{
+        let filteredData = [];
+
+        if (show === 'active') {
+          filteredData = inputData.filter(data => data.status === 'active');
+        } else if (show === 'completed') {
+          filteredData = inputData.filter(data => data.status === 'completed');
+        } else {
+          const activeStatus = inputData.filter(data => data.status === 'active');
+          const completedStates = inputData.filter(data => data.status === 'completed');
+          const otherStates = inputData.filter(data => data.status !== 'active' && data.status !== 'completed');
+          filteredData = [...activeStatus, ...completedStates, ...otherStates];
+        }
+
+        setInputData(filterData)
+
+    }, [show])
+ 
+    console.log(inputData , ".........................")
+
 
     return (
 
@@ -14,12 +49,12 @@ const Problem1 = () => {
             <div className="row justify-content-center mt-5">
                 <h4 className='text-center text-uppercase mb-5'>Problem-1</h4>
                 <div className="col-6 ">
-                    <form className="row gy-2 gx-3 align-items-center mb-4">
+                    <form onSubmit={handelInputValue} className="row gy-2 gx-3 align-items-center mb-4">
                         <div className="col-auto">
-                            <input type="text" className="form-control" placeholder="Name"/>
+                            <input name='name' type="text" className="form-control" placeholder="Name"/>
                         </div>
                         <div className="col-auto">
-                            <input type="text" className="form-control" placeholder="Status"/>
+                            <input name='status' type="text" className="form-control" placeholder="Status"/>
                         </div>
                         <div className="col-auto">
                             <button type="submit" className="btn btn-primary">Submit</button>
@@ -35,7 +70,7 @@ const Problem1 = () => {
                             <button className={`nav-link ${show==='active' && 'active'}`} type="button" onClick={()=>handleClick('active')}>Active</button>
                         </li>
                         <li className="nav-item">
-                            <button  className={`nav-link ${show==='completed' && 'active'}`} type="button" onClick={()=>handleClick('completed')}>Completed</button>
+                            <button  className={`nav-link ${show==='completed' && 'active'}`} type="button" onClick={()=>handleClick('completed')}>completed</button>
                         </li>
                     </ul>
                     <div className="tab-content"></div>
@@ -47,7 +82,7 @@ const Problem1 = () => {
                         </tr>
                         </thead>
                         <tbody>
-                        
+                            
                         </tbody>
                     </table>
                 </div>
